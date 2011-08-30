c46551b16912a7f08f4e7248512facae5dc93e2b
Matt Hisamoto <mwhisamoto@gmail.com>
2011-08-30 17:06:04 -0400

    I dont know but it hadnt been committed

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -4,12 +4,15 @@ class TestController < ApplicationController
   
   def show
     # @track = @client.get('/resolve', :url => "http://soundcloud.com/festapush/ringtone-push-salt-n-pepa")
-    @tracklist = @client.videos_by(:query => "push it", :per_page => 10)
+   @tracks = @client.videos_by(:query => "push it", :per_page => 1, :no_embed => false).videos
   end
   
   def search
     # @tracks = @client.get('/tracks', :limit => "10", :q => params[:terms])
     # @keyword = params[:terms]
+    
+    @tracks = @client.videos_by(:query => params[:terms], :per_page => 10, :no_embed => false).videos
+    
   end
     
 end
