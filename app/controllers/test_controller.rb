b2256e03f9f74e5e2755605a19442fefa92b3608
Matt Hisamoto <mwhisamoto@gmail.com>
2011-08-31 11:56:28 -0400

    added Capistrano

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,18 +1,14 @@
 class TestController < ApplicationController
   
-  before_filter :auth_youtube
+  before_filter :auth_soundcloud
   
   def show
-    # @track = @client.get('/resolve', :url => "http://soundcloud.com/festapush/ringtone-push-salt-n-pepa")
-   @tracks = @client.videos_by(:query => "push it", :per_page => 1, :no_embed => false).videos
+    @track = @client.get('/resolve', :url => "http://soundcloud.com/festapush/ringtone-push-salt-n-pepa")
   end
   
   def search
-    # @tracks = @client.get('/tracks', :limit => "10", :q => params[:terms])
-    # @keyword = params[:terms]
-    
-    @tracks = @client.videos_by(:query => params[:terms], :per_page => 10, :no_embed => false).videos
-    
+    @tracks = @client.get('/tracks', :limit => "10", :q => params[:terms])
+    @keyword = params[:terms]
   end
     
 end
---
+++ config/deploy.rb
@@ -0,0 +1,34 @@
+set :application, "pushitrealgood"
+set :repository,  "git@github.com:matthisamoto/push-it-real-good.git"
+
+set :default_environment, {
+  'PATH' => "/home/matt/.rvm/rubies/ruby-1.9.2-p180/bin:/home/matt/.rvm/gems/ruby-1.9.2-p180/bin:$PATH",
+  'RUBY_VERSION' => 'ruby 1.9.2',
+  'GEM_HOME'     => '/home/matt/.rvm/gems/ruby-1.9.2-p180/',
+  'GEM_PATH'     => '/home/matt/.rvm/gems/ruby-1.9.2-p180/',
+  'BUNDLE_PATH'  => '/home/matt/.rvm/gems/ruby-1.9.2-p180/'
+}
+
+set :scm, :git
+set :branch, 'master'
+set :user, 'matt'
+
+set :use_sudo, false
+set :deploy_to, "/home/matt/sites/#{application}"
+set :deploy_via, :copy
+set :copy_remote_dir, "/home/matt/sites/#{application}/tmp"
+set :copy_exclude, [".git"]
+
+role :web, 'pushitrealgood.com'                          # Your HTTP server, Apache/etc
+role :app, 'pushitrealgood.com'                          # This may be the same as your `Web` server
+role :db,  'pushitrealgood.com', :primary => true # This is where Rails migrations will run
+
+set :deploy_to, '/home/matt/sites/#{application}'
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
