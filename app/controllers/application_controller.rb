74513472cc820fee19a793ac67d635d10331cfd5
Matt Hisamoto <mwhisamoto@gmail.com>
2011-09-02 15:40:58 -0400

    changed path after sign-in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,15 @@ class ApplicationController < ActionController::Base
      def auth_soundcloud
        @client = Soundcloud.new(:client_id => "72325d0b84c6a7f4bbef4dd86c0a5309", :client_secret => "9e8d9f3e3559588aaf724007c69172ab" )
      end
-
+     
+     def after_sign_in_path_for(resource)
+       "page#user" # <- Path you want to redirect the user to after signup
+     end
+     
+     def after_sign_up_path_for(resource)
+       "page#user" # <- Path you want to redirect the user to after signup
+     end
+     
   layout :layout_by_resource
 
   protected
