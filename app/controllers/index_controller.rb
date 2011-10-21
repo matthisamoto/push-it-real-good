985fa98008668e2888492777316eb7bb4dbae5bb
Matt Hisamoto <mwhisamoto@gmail.com>
2011-10-21 16:52:01 -0400

    styled top tens

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,7 +3,7 @@ class IndexController < ApplicationController
   before_filter :get_count
   
   def index
-    @top_ten = Page.order("pages.pushes DESC", :limit => 10)
+    @top_ten = Page.order("pages.pushes DESC", :conditions => { :limit => 10 } )
     @feature = Page.find(:all).sample
   end
     
