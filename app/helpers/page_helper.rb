1a5839c7e3966bd513d15568190fc6d9325679af
Matt Hisamoto <mwhisamoto@gmail.com>
2011-10-23 21:11:24 -0400

    iphone

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -1,2 +1,15 @@
 module PageHelper
+  
+  def iphone?
+    request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(iPhone|iPod|iPad)/]
+  end
+  
+  def ie?
+    request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(MSIE)/]
+  end
+  
+  def android?
+    request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(Android)/]
+  end
+  
 end
