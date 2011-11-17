fec9ead7ac4c363276c724e406392d6c6ec4e434
Matt Hisamoto <mwhisamoto@gmail.com>
2011-11-17 00:52:29 -0500

    adjusting to absolute

--- public/js/new-page.js
+++ public/js/new-page.js
@@ -336,7 +336,7 @@ function initUploadSection() {
   ifrm.setAttribute("id", "log-in")
   ifrm.style.width = 456+"px";
   ifrm.style.height = 510+"px"; 
-  document.getElementById('content').appendChild(ifrm);
+  document.getElementById('content').prependChild(ifrm);
   ifrm.style.margin = "0 auto";
 
   /*
