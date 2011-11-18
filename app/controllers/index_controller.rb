54865fe88e1a5d7f1a2d52d1467a7dee4417647f
Matt Hisamoto <mwhisamoto@gmail.com>
2011-11-18 16:57:26 -0500

    example on home page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -5,6 +5,7 @@ class IndexController < ApplicationController
   def index
     @top_ten = Page.find(:all, :limit => 6, :order => "pages.pushes DESC" )
     @feature = Page.find(:first, :conditions => { :feature_time => Date.today })
+    @most_recent = Page.find(:first, :order => "created_at DESC" )
   end
     
 end
